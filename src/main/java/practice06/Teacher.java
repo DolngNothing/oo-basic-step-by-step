aa0ef64e0de4a94a888eb7bf94f2fd5a9b887a14
CHRISTOPHER LI (ITA-ISDC-ISD-OOCLL/ZHA) <LICH18@OOCL.COM>
2020-07-22 15:41:23 +0800

    [christopher] feature: finish practice6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer Klass;
+
+    public Integer getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(Integer klass) {
+        Klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        Klass = klass;
+    }
+    @Override
+    public String introduce(){
+        if(this.Klass!=null)
+        return super.introduce()+" "+new String("I am a Teacher. I teach Class "+this.Klass+".");
+        return super.introduce()+" "+new String("I am a Teacher. I teach No Class.");
+    }
+
 }
