f82eb51e8e025565826b690761f254d7ce1678de
CHRISTOPHER LI (ITA-ISDC-ISD-OOCLL/ZHA) <LICH18@OOCL.COM>
2020-07-22 15:26:50 +0800

    [christopher] feature: finish practice2

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,24 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int Klass;
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        return new String("I am a Student. I am at Class "+this.Klass +".");
+    }
+
 }
