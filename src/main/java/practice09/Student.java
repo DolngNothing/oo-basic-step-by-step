8507bcbdc43edb210396a3eda3afd936c7607d3c
CHRISTOPHER LI (ITA-ISDC-ISD-OOCLL/ZHA) <LICH18@OOCL.COM>
2020-07-22 16:26:36 +0800

    [christopher] feature: finish practice9

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,36 @@
 package practice09;
 
-public class Student {
+import java.util.Objects;
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(Integer id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(Integer id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        StringBuffer result=new StringBuffer(super.introduce()+" "+"I am a Student.");
+        if(klass.getLeader()==this){
+            result.append(" I am Leader of Class "+this.klass+".");
+        }else{
+            result.append(" I am at Class "+this.klass+".");
+        }
+        return result.toString();
+    }
+
 }
