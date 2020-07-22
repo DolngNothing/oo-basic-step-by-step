f9612a6441bcf92020f1590d305481ea9a658c1a
CHRISTOPHER LI (ITA-ISDC-ISD-OOCLL/ZHA) <LICH18@OOCL.COM>
2020-07-22 15:57:30 +0800

    [christopher] feature: finish practice7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,40 @@
 package practice07;
 
+import java.util.Objects;
+
 public class Klass {
+    private Integer klass;
+
+    public Klass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String toString() {
+        return klass.toString();
+    }
+
+    public Integer getNumber(){
+        return klass;
+    }
+
+    public String getDisplayName(){
+        if(klass!=null){
+            return "Class "+klass;
+        }
+        return "No Class";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass1 = (Klass) o;
+        return Objects.equals(klass, klass1.klass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(klass);
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,27 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce()+" "+new String("I am a Student. I am at Class "+this.klass +".");
+    }
 }
