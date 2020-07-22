efc7ede1d2e289c8fab317b956477c2a3f25a947
CHRISTOPHER LI (ITA-ISDC-ISD-OOCLL/ZHA) <LICH18@OOCL.COM>
2020-07-22 16:13:17 +0800

    [christopher] feature: finish practice8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,62 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Klass {
+    private Integer klass;
+
+    private Student leader;
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Klass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String toString() {
+        return klass.toString();
+    }
+
+    public Integer getNumber(){
+        return klass;
+    }
+
+    public String getDisplayName(){
+        if(klass!=null){
+            return "Class "+klass;
+        }
+        return "No Class";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass1 = (Klass) o;
+        return Objects.equals(klass, klass1.klass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(klass);
+    }
+
+    public void assignLeader(Student leader){
+        this.leader=leader;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,40 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(Integer id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+
+    @Override
+    public String introduce() {
+        if (this.klass != null)
+            return super.introduce() + " " + new String("I am a Teacher. I teach Class " + this.klass + ".");
+        return super.introduce() + " " + new String("I am a Teacher. I teach No Class.");
+    }
+
+    public String introduceWith(Student student){
+        StringBuffer result=new StringBuffer("My name is "+this.name+". I am "+this.age+" years old. I am a Teacher.");
+        if(student.getKlass()==this.klass){
+            result.append(" I teach "+student.getName()+".");
+        }else {
+            result.append(" I don't teach "+student.getName()+".");
+        }
+        return result.toString();
+    }
 }
