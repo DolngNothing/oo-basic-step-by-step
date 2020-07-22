03e32349f0ae84a69e183133b7059c132238c465
CHRISTOPHER LI (ITA-ISDC-ISD-OOCLL/ZHA) <LICH18@OOCL.COM>
2020-07-22 17:34:27 +0800

    [christopher] feature: finish practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,82 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Klass {
+    private Integer klass;
+
+    private Student leader;
+
+    private List<Student> members =new ArrayList<Student>();
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Klass(Integer klass) {
+        this.klass = klass;
+    }
+
+    public List<Student> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Student> members) {
+        this.members = members;
+    }
+
+    public void appendMember(Student student){
+        members.add(student);
+    }
+
+    @Override
+    public String toString() {
+        return klass.toString();
+    }
+
+    public Integer getNumber(){
+        return klass;
+    }
+
+    public String getDisplayName(){
+        if(klass!=null){
+            return "Class "+klass;
+        }
+        return "No Class";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass1 = (Klass) o;
+        return Objects.equals(klass, klass1.klass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(klass);
+    }
+
+    public void assignLeader(Student leader){
+        if(members.contains(leader))
+            this.leader=leader;
+        else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,56 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> klasses;
+
+    public Teacher(Integer id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, int age, LinkedList<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return klasses;
+    }
+
+    public void setKlasses(LinkedList<Klass> klasses) {
+        this.klasses = klasses;
+    }
+
+    @Override
+    public String introduce() {
+        StringBuffer result = new StringBuffer(super.introduce() + " "+"I am a Teacher. I teach Class ");
+        if (this.klasses!=null&&!this.klasses.isEmpty()){
+            for(int i=0;i<klasses.size();i++) {
+                if (i != klasses.size() - 1)
+                    result.append(klasses.get(i) + ", ");
+                else result.append(klasses.get(i));
+            }
+            result.append(".");
+            return result.toString();
+        }
+        return super.introduce() + " " + new String("I am a Teacher. I teach No Class.");
+    }
+
+    public String introduceWith(Student student){
+        StringBuffer result=new StringBuffer("My name is "+this.name+". I am "+this.age+" years old. I am a Teacher.");
+        if(klasses.contains(student.getKlass())){
+            result.append(" I teach "+student.getName()+".");
+        }else {
+            result.append(" I don't teach "+student.getName()+".");
+        }
+        return result.toString();
+    }
+
+    public boolean isTeaching(Student student){
+        if(klasses.contains(student.getKlass())){
+            return true;
+        }
+        return false;
+    }
 }
